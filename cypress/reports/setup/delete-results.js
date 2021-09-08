685be174bf79e8aa3ec3cd5835a33aa410a57cfa
carlos-pardo <carlos.pardo@usfoods.com>
2021-09-08 13:49:01 -0300

    - Removed unnecessary code

--- cypress/reports/setup/delete-results.js
+++ cypress/reports/setup/delete-results.js
@@ -2,18 +2,8 @@
 const rimraf = require('rimraf')
 const chalk = require('chalk')
 
-const testResultsDir = '../results/cypress/reports/test-results/test-*'
-const screenshotsResultsDir = '../results/cypress/screenshots/*'
-const videosResultsDir = './cypress/videos/*'
+const testResultsDir = '../results'
 
 rimraf(testResultsDir, () => {
   console.info(chalk.yellow(`    Deleted former test results on : ${testResultsDir}`))
-})
-
-rimraf(screenshotsResultsDir, () => {
-  console.info(chalk.yellow(`    Deleted former test screenshots on : ${screenshotsResultsDir}`))
-})
-
-rimraf(videosResultsDir, () => {
-  console.info(chalk.yellow(`    Deleted former test videos on : ${videosResultsDir}`))
 })
