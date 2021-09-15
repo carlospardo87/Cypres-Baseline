7811bc0ad96a3e680f19dbc10f41db48b800b3c4
carlos-pardo <carlos.pardo@usfoods.com>
2021-09-15 18:20:01 -0300

    Added new path to delete resources

--- cypress/reports/setup/delete-results.js
+++ cypress/reports/setup/delete-results.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+//     #!/bin/bash
 const rimraf = require('rimraf')
 const chalk = require('chalk')
 
-const testResultsDir = '../results'
+const testResultsDir = '../results/cypress/*'
 
 rimraf(testResultsDir, () => {
   console.info(chalk.yellow(`    Deleted former test results on : ${testResultsDir}`))
