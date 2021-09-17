f59f6dd5a5debb786724f8b86bca1022b0ea6df5
carlos-pardo <carlos.pardo@usfoods.com>
2021-09-17 12:56:56 -0300

    Updated sending js file email with new features

--- cypress/reports/setup/sendEmail.js
+++ cypress/reports/setup/sendEmail.js
@@ -1,6 +1,33 @@
+'use strict'
 
 const nodemailer = require("nodemailer");
 const chalk = require("chalk");
+const fs = require('fs')
+
+
+const resultInfo = () => {
+try {
+  let rawData =  fs.readFileSync('cypress/support/storeResult.js', 'utf8');
+  return JSON.parse(rawData)
+} catch (err) {
+  console.error(err)
+  return false
+}
+}
+
+const percentSuccess = () => {
+  let a = parseFloat(resultInfo().totalPassed)*100
+  return (a/parseFloat(resultInfo().totalTests)).toFixed(2)
+}
+
+const envType = () => {
+  if (resultInfo().config.baseUrl.includes('sit')) {
+    return 'SIT'
+  }else {
+    return 'DEV'
+  }
+}
+
 
 let transporter = nodemailer.createTransport({
   host: "smtp.usfood.com",    
@@ -18,19 +45,49 @@ let transporter = nodemailer.createTransport({
 
 let mailOptions = {
   from: '"Automation Report 👻" <foo@usfoods.com>',
-  to:  'carlos.pardo@usfoods.com' ,//'2S-DL-R4Ordering@usfood.com,2S-DL-Panamax@usfood.com,2S-DL-R4List@usfood.com,2S-DL-R4ProductDiscovery@usfood.com,2S-DL-R4Alerts@usfood.com',
+  to: 'carlos.pardo@usfoods.com, bab5fc4c.usfoods.onmicrosoft.com@amer.teams.ms',//'2S-DL-R4Ordering@usfood.com,2S-DL-Panamax@usfood.com,2S-DL-R4List@usfood.com,2S-DL-R4ProductDiscovery@usfood.com,2S-DL-R4Alerts@usfood.com',
   subject: 'Automation report',
-  text: "Automated testing report - List Management Team",
-  html: '<b> 🚀    List Management Team    🚀</b>',
- // attachments: [{
- //   path: '../results/cypress/reports/test-results/report.html'}]
+  text: 'Automation',
+  html: `<b> 👉🏻  <i>List Management Team</i>  👈🏻 </b><br>
+   <b>  ------------------------------------------------------------------------------------------------------------- </b>
+  
+  <table class="default">
+  <tr>
+    <th>| Total_Tests | </th><th> Total_Passed |</th><th> Total_Failed |</th><th> Browser_Name |</th><th> Environmet </th><th>| Viewport |</th><th> % Success |</th>
+  </tr>
+  <tr>
+    <td>${resultInfo().totalTests}</td>
+    
+    <td>${resultInfo().totalPassed}</td>
+    
+    <td>${resultInfo().totalFailed}</td>
+   
+    <td>${resultInfo().browserName}</td>
+    
+    <td>${envType()}</td>
+    
+    <td>${resultInfo().config.viewportWidth}x${resultInfo().config.viewportHeight}</td>
+    
+    <td>${percentSuccess()}</td>
+  </tr>
+</table>`,
+
+  attachments: [{
+    path: '../results/cypress/reports/report.html'}]
 };
+
+
 
 transporter.sendMail(mailOptions, function(error, info){
   if (error) {
     console.info(chalk.red(`    Error sending email: ${error}`))
   } else {
-    //console.log('Email sent: ' + info.response);
     console.info(chalk.green(`🚀     Report was sent by email successfully     👍`))
+    resultInfo()
   }
 });
+
+
+
+
+
