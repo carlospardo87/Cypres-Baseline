d1ed4169b6e53d87670d6794255cf6d52507b996
carlos-pardo <carlos.pardo@usfoods.com>
2021-10-01 16:02:55 -0300

    Updated api features and steps_definitions.

--- cypress/integration/step_definitions/api/post_pricing.js
+++ cypress/integration/step_definitions/api/post_pricing.js
@@ -15,15 +15,16 @@ When('product pricing number {string} was requested', (productNum) => {
   })
 })
 
-Then('product pricing response body should be successful', () => {
+Then('product pricing response body should contain {string}', (productNum) => {
   cy.get('@getProductPricing').then((response) => {
 
-    expect(response.body.messageHeader.divisionNumber).match(/^\d+$/, 'should be a number')
-    expect(response.body.messageHeader.customerNumber).match(/^\d+$/, 'should be a number')
+    expect(response.body.messageHeader.divisionNumber).match(/^\d+$/, 'DivisionNumber should be a number')
+    expect(response.body.messageHeader.customerNumber).match(/^\d+$/, 'Customer Number should be a number')
 
-    expect(response.body.messageDetail.productList[0].eachPrice).match(/^\w+$/, 'should be a string')
-    expect(response.body.messageDetail.productList[0].unitPrice).match(/^\w+$/, 'should be a string')
-    expect(response.body.messageDetail.productList[0].productNumber).match(/^\w+$/, 'should be a string')
+    expect(response.body.messageDetail.productList[0].productNumber).to.be.equal(productNum, 'Product Number should be equal')
+    expect(response.body.messageDetail.productList[0].eachPrice).match(/^\d+\.?\d*$/, 'Each Price should be a number')
+    expect(response.body.messageDetail.productList[0].unitPrice).match(/^\d+\.?\d*$/, 'Unit Price should be a number')
+
 
   })
 })
