9447b028f5cc9cfcdc70c74486c38c3ece2deedc
carlos-pardo <carlos.pardo@usfoods.com>
2021-10-15 19:48:05 -0300

    Refactors - Fixes

--- cypress/integration/step_definitions/ui/loginPage.js
+++ cypress/integration/step_definitions/ui/loginPage.js
@@ -4,11 +4,41 @@ import {Given, When} from "cypress-cucumber-preprocessor/steps";
 
 import LoginPage from '../../../support/pages/LoginPage';
 
-When("{string} user logs in with valid credentials {string} and {string}", (userType, uname, password) => {
+When("logs in with valid credentials {string} and {string}", (uname, password) => {
   new LoginPage().loggingIn(uname, password)
+});
+
+
+When("logs in with valid credentials", () => {
+  cy.fixture('login').then( (login) => {
+    new LoginPage().loggingIn(login.internal.user, login.internal.password)
+  })
+});
+
+When("External user logs in with valid credentials", () => {
+  cy.fixture('login').then( (login) => {
+    new LoginPage().loggingIn(login.external.user, login.external.password)
+  })
 });
 
 
 Given("User navigates to USF with {string}", (device) => {
   new LoginPage().navigateWithViewPort(device)
+});
+
+
+Given("{string} user navigates to USF and logs in", (userType) => {
+
+  if (userType === 'Internal') {
+    cy.fixture('login').then( (login) => {
+      new LoginPage().navigateWithViewPort(login.device)
+      new LoginPage().loggingIn(login.internal.user, login.internal.password)
+    })
+  }else {
+    cy.fixture('login').then( (login) => {
+      new LoginPage().navigateWithViewPort(login.device)
+      new LoginPage().loggingIn(login.external.user, login.external.password)
+    })
+  }
+
 });
